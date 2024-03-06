2c2f9a8c44d33495f5f9a9526810308bd00eaa70
ありす <master@16439s.jp>
2024-03-06 16:31:21 +0900

    Backend: LOGINエンドポイント追加

--- server.ts
+++ server.ts
@@ -1,12 +1,67 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const app = express();
 const PORT = 3000;
 
+const appname = "Vocaloid.social Timeline";
+const instance = "vocaloid.social";
+const callback = "client.164.one";
+const permissions = "read:account,write:notes";
+
 // index.htmlを表示する
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/login', (req: Request, res: Response) => {
+    if (req.cookies.token) {
+        res.send("既にログインされています。<br>3秒後にトップページに戻ります。");
+        setTimeout(() => {
+            res.redirect('/index.php');
+        }, 3000);
+    } else {
+        const uuid = uuidv4();
+        const url = `https://${instance}/miauth/${uuid}?name=${appname}&callback=https://${callback}/callback.php&permission=${permissions}`;
+        res.redirect(url);
+    }
+});
+
+app.get('/api/login/callback', async (req: Request, res: Response) => {
+    if (req.query.session) {
+        const session = req.query.session as string;
+        const host = req.get('referer');
+        const url = `${host}/api/miauth/${session}/check`;
+
+        try {
+            const { data } = await axios.post(url, {}, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (data.ok && data.token) {
+                res.cookie('token', data.token, { maxAge: 60 * 60 * 24 * 7 * 1000 });
+                const userName = data.user.name;
+                res.send(`<div id='login-message' style='display:none;'>
+                            ${userName} にログインしました。<br>5秒後にトップページに戻ります。
+                        </div>
+                        <script>
+                            setTimeout(() => {
+                                location.href = '/';
+                            }, 5000);
+                        </script>`);
+            } else {
+                res.redirect('/api/login');
+            }
+        } catch (error) {
+            res.redirect('/api/login');
+        }
+    } else {
+        res.redirect('/api/login');
+    }
 });
 
 // 静的ファイルの提供
